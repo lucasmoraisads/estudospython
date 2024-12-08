ce4c603418d79c421ce39e38d7df38cdae139416
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-08 13:52:20 -0300

    Update 18-list_compehension.py

--- fundamentos/18-list_compehension.py
+++ fundamentos/18-list_compehension.py
@@ -1,5 +1,18 @@
 # 1 listando numeros menores que 4 de 0 a 10
 
-for i in range(10):
-    if  i < 4 :
-        print(i)    
+listaNumero = [ i for i in range(10) if i < 4]
+
+print(listaNumero)
+
+# lista filme
+
+filmesList = ["Titanic", "Nimona", "Deadpool", "Mascara"]
+
+# 2 filtrando por letra 
+
+filtro_letra = [filme for filme in filmesList if 'i' in filme.lower() ]
+
+print(filtro_letra)
+
+# 3 filmes que eu assisti 
+
