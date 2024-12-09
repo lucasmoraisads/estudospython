69fd79ba46855e0bdc695c467b92d8c1aae3363f
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-09 08:03:06 -0300

    funções

--- fundamentos/19-funcao.py
+++ fundamentos/19-funcao.py
@@ -8,25 +8,31 @@ def olar():
 
 # 2 Função para calcular a media de notas
 
-def calcular():
-    numAvalicoes = int(input("Digite quantas avaliações deseja fazer para o filme: "))
-    total = 0
-    for i in range(numAvalicoes):
-        note = float(input("Digite a nota para o filme: "))
-        total += note
+# def calcular():
+#     numAvalicoes = int(input("Digite quantas avaliações deseja fazer para o filme: "))
+#     total = 0
+#     for i in range(numAvalicoes):
+#         note = float(input("Digite a nota para o filme: "))
+#         total += note
 
-    if numAvalicoes > 0 :
-        media = total / numAvalicoes
-    else:
-        media = 0
+#     if numAvalicoes > 0 :
+#         media = total / numAvalicoes
+#     else:
+#         media = 0
 
-    return media
+#     return media
 
-print(f"A media de avaliações é: {calcular():.2f}")
+# print(f"A media de avaliações é: {calcular():.2f}")
 
 # 3 função para cadastra um filme
 
 def cadastraFilme():
-    nome = input("Digite o nome do Filme:")
-    anoLancamento = int(input("Digite o ano de lançamento:"))
-    note = float(input("Digite Uma nota:"))
+    nome = input("Digite o nome do Filme: ")
+    anoLancamento = int(input("Digite o ano de lançamento: "))
+    precoFilme = float(input("Digite o valor do Filme: "))
+    notaFilme = float(input("Digite a nota Do filme: "))
+
+    print(f"{nome} - ({anoLancamento}) - R${precoFilme}")
+
+cadastraFilme()
+cadastraFilme()
