ef762b94cc54f4b043f2f47843c50d05ca12ecb3
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-09 07:54:40 -0300

    Nomeclatura
    refatorando

--- fundamentos/18-list_compehension.py
+++ fundamentos/18-list_compehension.py
@@ -19,3 +19,19 @@ print(filtro_letra)
 filmeAssisti = [ filme for filme in filmesList if filme != "Mascara"] 
 
 print(filmeAssisti)
+
+# 4 encontrando o filme pole nome
+
+while True:
+    buscarFilme = input("Digite o nome do filme para busca na lista ( ou sair para encerrar ): \n")
+    if buscarFilme.lower() == "sair":
+        print("Programa encerrado") 
+        break
+
+    encontraFilme = [ filme for filme in filmesList if buscarFilme.lower() in filme.lower()]
+    if encontraFilme:
+        print(f"Filme(s) encontrado(s) com o nome: {buscarFilme}")
+        for encontraFilme in encontraFilme:
+            print(encontraFilme)
+    else:
+        print(f"Nenhum filme encontrado com o nome {buscarFilme}. Tente Novamente")
---
+++ fundamentos/19-funcao.py
@@ -0,0 +1,30 @@
+# função para imprimir uma mensagem
+
+def olar():
+    print("Bem Vindo ao Sistema de Filme")
+
+# for i in range(10):
+#     olar()
+
+# 2 Função para calcular a media de notas
+
+def calcular():
+    numAvalicoes = int(input("Digite quantas avaliações deseja fazer para o filme: "))
+    total = 0
+    for i in range(numAvalicoes):
+        note = float(input("Digite a nota para o filme: "))
+        total += note
+
+    if numAvalicoes > 0 :
+        media = total / numAvalicoes
+    else:
+        media = 0
+
+    return media
+
+print(f"A media de avaliações é: {calcular():.2f}")
+
+# 3 função para cadastra um filme
+
+def cadastraFilme():
+    
