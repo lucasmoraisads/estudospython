67f2ac6c734a04c153bac744bf4df4f80e164bfe
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-09 07:55:07 -0300

    Update 19-funcao.py

--- fundamentos/19-funcao.py
+++ fundamentos/19-funcao.py
@@ -27,4 +27,6 @@ print(f"A media de avaliações é: {calcular():.2f}")
 # 3 função para cadastra um filme
 
 def cadastraFilme():
-    
+    nome = input("Digite o nome do Filme:")
+    anoLancamento = int(input("Digite o ano de lançamento:"))
+    note = float(input("Digite Uma nota:"))
