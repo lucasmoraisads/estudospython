1210d4b8bbde62447d6e2e571250122e44d0ab85
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-10 09:22:30 -0300

    Update 23-la,bda-funcao.py

--- fundamentos/23-la,bda-funcao.py
+++ fundamentos/23-la,bda-funcao.py
@@ -8,9 +8,34 @@ par = lambda x: x % 2 == 0
 
 #  Função que divide um numero por outro
 
+divide = lambda x, y: x / y
+
+# função que inverte uma string 
+
+inverter = lambda s: s[::-1]
+
 
 print(power(5))
 print(power(8))
 print()
 print(par(23))
 print(par(20))
+print()
+print(divide(10, 3))
+print(divide(100, 5))
+print()
+print(inverter("Lucas"))
+
+
+
+# funcionalidades relacionadas a filmes
+
+filmeList = ["Mascara", "Nimona", "Esquadrão Classe A", "Os Simpsons", "Como treinar seu dragão", "trancendente"]
+notaFilme = {
+    "Mascara" : [7.8, 5.7, 8.9],
+    "Nimona" : [8.8, 7.7, 8.9],
+    "Esquadrão Classe A" : [9.8, 8.7, 10],
+    "Os Simpsons" : [9.0, 8.9, 8.9],
+    "Como treinar seu dragão" : [7.8, 9.7, 8.9],
+    "trancendente" : [6.8, 8.7, 9.9],
+}
