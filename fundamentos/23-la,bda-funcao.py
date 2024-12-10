b5ae07c43a72320b21056d913e721f34dc9a02e3
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-10 09:07:15 -0300

    função lambda
    Verifica se e par

--- fundamentos/23-la,bda-funcao.py
+++ fundamentos/23-la,bda-funcao.py
@@ -2,4 +2,13 @@
 
 power = lambda num: num ** 2
 
+# 2 função que verifica se o numero e par
+
+par = lambda x: x % 2 == 0
+
+# 
 print(power(5))
+print(power(8))
+print()
+print(par(23))
+print(par(20))
