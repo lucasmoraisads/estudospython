e9ba567c0de71f96a56346522dec6b251f68d91a
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-10 17:02:46 -0300

    Update math_operation.py

--- modulos/math_operation.py
+++ modulos/math_operation.py
@@ -1,1 +1,17 @@
 # modelo de operações matematicas
+
+def sum(x, y):
+    return x + y
+
+def subtrair(x , y):
+    return x - y
+
+def mutipli(x , y):
+    return x * y
+
+def div(x , y):
+    if y != 0:
+        return  x / y
+    else:
+        raise ValueError("Não e permitido divisão por Zero")
+    
