c4b3b3d4178d200b76f613842b3f96dcb810508b
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-10 07:34:19 -0300

    Update 22-funcoes_args_kewargs.py

--- fundamentos/22-funcoes_args_kewargs.py
+++ fundamentos/22-funcoes_args_kewargs.py
@@ -1,6 +1,8 @@
 """
 * args - utilizamos quendo não sabemos quantos argumento queremos em uma função
-* 0s argumentos são pasados com uma tupla
+- 0s argumentos são pasados com uma tupla
+** alem dos valores podemos passar tambem as respectivas chave para cada argumento
+- Os argumento são pasado como um dicionario
 """
 
 def sum (*num):
@@ -12,3 +14,14 @@ def sum (*num):
 
 sum(7)
 sum(3,56, 67, 88)
+
+# 2 Apresentação de cursos
+
+def presentacao(**data):
+    for key, value in data.items():
+        print(f"{key} - {value}")
+
+print("Lista de Cursos")
+presentacao(nome="Python", Categoria="Beck-end", nivel="Iniciante")
+presentacao(nome="JavaScript", Categoria="Beck-end", nivel="Médio")
+presentacao(nome="Rede de Computadores", Categoria="TI", nivel="Avançado")
