b4272ecf9d0a292620be1661e610d3e27524aa72
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-10 17:12:37 -0300

    Update main.py

--- modulos/main.py
+++ modulos/main.py
@@ -1,9 +1,10 @@
 # programa principal
 import math_operation
 from math_operation import mutipli, div
-
+import strint_ultils
 
 print(math_operation.sum(5,9))
 print(math_operation.subtrair(5,3))
 print(mutipli(5,9))
 print(div(18,9))
+
