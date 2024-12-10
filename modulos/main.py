5d52f4a034b549ed184036c32314dba2f768f739
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-10 17:08:57 -0300

    sobre modulo
    as importações sobre modulos

--- modulos/main.py
+++ modulos/main.py
@@ -1,1 +1,9 @@
 # programa principal
+import math_operation
+from math_operation import mutipli, div
+
+
+print(math_operation.sum(5,9))
+print(math_operation.subtrair(5,3))
+print(mutipli(5,9))
+print(div(18,9))
