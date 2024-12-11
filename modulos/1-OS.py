d448e28bb91afaf2eb52a9fff4e4c11d2bffcb53
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-11 09:57:39 -0300

    modulo OS

---
+++ modulos/1-OS.py
@@ -0,0 +1,27 @@
+import os
+
+# 1 retorna a pasta atual
+print(os.getcwd())
+
+# 2 listar arquivos e pastas 
+
+print(os.listdir())
+
+# 3 versão do sistema
+
+os.system('ver')
+
+# 4 tras as configurações da maquina
+
+os.system('systeminfo')
+
+# 5 limpar a tela do terminal
+
+os.system('cls')
+
+# 6 desligar o computador
+
+# os.system('shutdown /s')
+# os.system('shutdown /s /t 0') desliga imediatamente
+os.system('shutdown /a')
+
