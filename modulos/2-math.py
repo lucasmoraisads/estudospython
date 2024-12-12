4c58f6b643f4e1089d776ce01fa64375124ceef6
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-12 10:19:36 -0300

    Update 2-math.py

--- modulos/2-math.py
+++ modulos/2-math.py
@@ -4,3 +4,19 @@ import math
 
 print(math.pi)
 print(f"{math.pi:.2f}")
+
+# 2 acessar o numero de euler
+
+print(math.e)
+print(f"{math.e:.2f}")
+
+# 3 arredondamento de numero tanto para cima quanto para baixo
+
+num = 10.6
+print(math.ceil(num))
+print(math.floor(num)) 
+
+# 4 fatorial de um numero
+
+num2 = int(input("Digite um número: "))
+print(math.factorial(num2))
