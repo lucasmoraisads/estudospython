d822b4a0e972f49faa729302e6070b94a9ab594e
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-13 08:54:36 -0300

    Update 3-statics.py

--- modulos/3-statics.py
+++ modulos/3-statics.py
@@ -6,3 +6,9 @@ print(statistics.mean([23, 45, 67, 33, 87, 40]))
 
 # 2 aplicando a mediana 
 
+print(statistics.median([2, 5, 6, 8, 23, 78, 45, 98, 34, 36]))
+print(statistics.median([1, 2, 3, 5, 7, 9]))
+
+# 3 aplicando a moda
+
+print(statistics.mode([2, 3, 6, 5, 2,4, 8, 4, 9, 2]))
