9be9d6e72fb0f8a3d714e13eaf057dd23ec16e81
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-13 09:50:49 -0300

    Update 5-hashlib.py

--- modulos/5-hashlib.py
+++ modulos/5-hashlib.py
@@ -7,3 +7,17 @@ print(hashlib.algorithms_available)
 # 2- verificar algoritmos de acordo com o SO
 
 print(hashlib.algorithms_guaranteed)
+
+# 3 utilizando o SHA256
+
+algoritmo = hashlib.sha256()
+print(algoritmo.digest())
+mensagem = "A melhor forma de prover o futuro é crialo".encode()
+algoritmo.update(mensagem)
+print(algoritmo.hexdigest())
+
+# 4 utlizando o MD5
+
+md5 = hashlib.md5()
+md5.update(mensagem)
+print(md5.hexdigest())
