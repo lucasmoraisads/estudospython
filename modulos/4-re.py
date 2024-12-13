c0f309ec5826bd35f9c16a937dd5fc1b60e0a959
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-13 09:19:28 -0300

    Update 4-re.py

--- modulos/4-re.py
+++ modulos/4-re.py
@@ -1,2 +1,21 @@
 import re
 
+text = "Udemy - uma plantaforma com muitos cursos"
+# 1 - mapear indice inicial e indice final
+# o r significa uma raw string (string bruta)
+
+match = re.search(r'uma plantaforma', text)   
+print(f"Indice inicial: {match.start()}")
+print(f"Indice final: {match.end()}")
+
+# 2 - Buscando o índice que possui ponto
+
+site = 'https://udemy.com.br'
+match = re.search(r'\.', site)
+print(match)
+
+# 3 - Buscar uma lista de caracteres dentro de uma frase 
+
+pattern = "[g-m]"
+resultado = re.findall(pattern, text)
+print(resultado)
