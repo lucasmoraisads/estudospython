42b9c900108fd0cf222665cc2e614db3616740f7
Lucas Morais <118077670+lucasmoraisads@users.noreply.github.com>
2024-12-15 08:20:40 -0300

    Update kinter.py

--- modulos/kinter.py
+++ modulos/kinter.py
@@ -15,5 +15,23 @@ frame.pack(padx=10, pady=10, fill='x',expand=True)
 label = tk.Label(frame, text="Óla Mundo")
 label.pack(fill='x', expand=True)
 
+# 4 adiconando o input texto
+
+frase = tk.Label(frame, text="Frase")
+frase.pack(fill='x', expand=True)
+
+frase_inp = tk.Entry(frame)
+frase_inp.pack(fill='x', expand=True)
+
+# 5 - função para altera o texto
+
+def Click():
+    label.config(text=frase_inp.get())
+
+
+# 6 adicionar botão
+
+button = tk.Button(frame, text="Enviar", command=Click)
+button.pack()
 
 janela.mainloop()
